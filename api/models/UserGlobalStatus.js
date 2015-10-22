a474171a0a8aab7eab6d1f13d00b5a5d9c7c3252
kmangla <karan.mangla@gmail.com>
2015-10-23 00:13:41 +0530

    Adding a function to this.

--- api/models/UserGlobalStatus.js
+++ api/models/UserGlobalStatus.js
@@ -28,6 +28,20 @@ module.exports = {
 
     replyPending: {
       type: 'boolean',
+    },
+
+    // Custom attributes
+
+    shouldSendReminderFromEmployer: function (user) {
+      if (!this.replyPending) {
+        return false;
+      }
+      if (Util.daysSince(new Date(), this.timeFirstReminderSent, user) > 7) {
+        if (Util.daysSince(new Date(), this.timeEmployeeSMSSent, user) > 7) {
+          return true;
+        }
+      }
+      return false;
     }
   }
 }
