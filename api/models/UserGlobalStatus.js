4df7aa9dedaa3cda8356417b9dd0494098c8c9d3
kmangla <karan.mangla@gmail.com>
2015-10-28 14:15:48 +0530

    Change messaging for Nag SMS.

--- api/models/UserGlobalStatus.js
+++ api/models/UserGlobalStatus.js
@@ -41,8 +41,8 @@ module.exports = {
       if (!((date.hour() >= 11) && (date.hour() <= 17) && (date.day() != 0) && (date.date() != 6))) {
         return false;
       }
-      if (Util.daysSince(new Date(), this.timeFirstReminderSent, user) > 6) {
-        if ((this.timeEmployeeSMSSent == null) || (Util.daysSince(new Date(), this.timeEmployeeSMSSent, user) > 6)) {
+      if (Util.daysSince(new Date(), this.timeFirstReminderSent, user) > 3) {
+        if ((this.timeEmployeeSMSSent == null) || (Util.daysSince(new Date(), this.timeEmployeeSMSSent, user) > 3)) {
           return true;
         }
       }
@@ -52,7 +52,7 @@ module.exports = {
 
   sendReminderFromEmployer: function (employer, employee, taskID, cb) {
     PushToken.findOrAssignToken(employee, function (err, token) {
-      var message = 'Please reply to reminder messages from ' + token.deviceID + ' with updates on tasks.';
+      var message = 'You will have received SMS\'s from ' + token.deviceID + '. Please reply to these messages with updates on tasks.';
       Logging.logInfo('employee_sms', employer.id, employee.id, taskID, message);
       if ((employer.id == '5548fbd7cd008003003f04ab') || employer.id == '5623a51e28c11d030061177e' || employer.id == '56174808f83347030087b707') {
         SendNotification.sendNotification(employer.id, employee.id, message, taskID, 'silentMessage', function () {
