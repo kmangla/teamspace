c300aad2a157de72c37f097c9662e674c7a42882
kmangla <karan.mangla@gmail.com>
2015-12-04 15:57:53 +0530

    Add more bylines.

--- api/services/MockMessage.js
+++ api/services/MockMessage.js
@@ -3,28 +3,28 @@ module.exports = {
     var taskIDstoMessage = {};
     Task.find().where({id: taskIDs}).populate('assignedBy').populate('assignedTo').populate('currentStatus').exec(function (err, tasks) {
       var userStatusesToFetch = {};
-      var dueTasks = [];
       for (var i = 0; i < tasks.length; i++) {
         var task = tasks[i];
         userStatusesToFetch[task.assignedTo.id] = 1;
-        if (task.reminderIsDue(task.assignedTo)) {
-          dueTasks.push(task);
-        }
       }
       UserStatus.find().where({user: Object.keys(userStatusesToFetch)}).exec(function (err, statuses) {
         var statusMap = Util.extractMap(statuses, "user");
         var userMapList = Util.extractMapList(dueTasks, "assignedTo", "id");
-        for (var i = 0; i < dueTasks.length; i++) {
-          var task = dueTasks[i];
+        for (var i = 0; i < tasks.length; i++) {
+          var task = tasks[i];
           var userID = task.assignedTo.id;
-          if (statusMap[userID].replyPending && statusMap[userID].taskSent == dueTasks[i].id) {
-            taskIDstoMessage[task.id] = MockMessage.createReminderCurrentlySentMessage(task);
-          } else {
-            if (task.currentStatus.replyPending) {
-              taskIDstoMessage[task.id] = MockMessage.createRepeatReminderWillBeSent(task);
+          if (task.reminderIsDue(task.assignedTo)) {
+            if (statusMap[userID].replyPending && statusMap[userID].taskSent == tasks[i].id) {
+              taskIDstoMessage[task.id] = MockMessage.createReminderCurrentlySentMessage(task);
             } else {
-              taskIDstoMessage[task.id] = MockMessage.createReminderWillBeSent(task);
+              if (task.currentStatus.replyPending) {
+                taskIDstoMessage[task.id] = MockMessage.createRepeatReminderWillBeSent(task);
+              } else {
+                taskIDstoMessage[task.id] = MockMessage.createReminderWillBeSent(task);
+              }
             }
+          } else {
+            taskIDstoMessage[task.id] = MockMessage.createUpdated(task);
           }
         }
         cb(null, taskIDstoMessage);
@@ -32,7 +32,20 @@ module.exports = {
     });
   },
 
-  createRepeatReminderWillBeSent: function (task, offset) {
+  createUpdated: function (task) {
+    var message = {
+      id: 'm_' + task.id,
+      description: 'Task is up to date',
+      forTask: task.id,
+      sentBy: task.assignedBy,
+      systemGenerated: true,
+      createdAt: task.lastUpdate,
+      updatedAt: task.lastUpdate
+    }; 
+    return message;
+  },
+
+  createRepeatReminderWillBeSent: function (task) {
     var message = {
       id: 'm_' + task.id,
       description: 'Reminders sent earlier. Total reminders ' + task.currentStatus.reminderCount,
@@ -45,7 +58,7 @@ module.exports = {
     return message;
   },
 
-  createReminderWillBeSent: function (task, offset) {
+  createReminderWillBeSent: function (task) {
     var message = {
       id: 'm_' + task.id,
       description: 'Reminder due. Reminder will be sent soon',
