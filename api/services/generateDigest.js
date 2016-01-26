8f70bdfe31e63f9ab81c4d694ad4ad957bf69101
kmangla <karan.mangla@gmail.com>
2016-01-26 20:07:31 +0530

    Fix digest notification code.

--- api/services/generateDigest.js
+++ api/services/generateDigest.js
@@ -87,7 +87,7 @@ module.exports = {
       if (tasks.length == 0) {
         var message = 
           'No tasks created in last two weeks. Create new tasks to monitor employees';
-        generateDigest.createDigest(user, digest, 'taskCreation', message, function () {
+        generateDigest.createDigest(user, digest, 'new_task', message, function () {
           SendNotification.sendNotification(user.id, user.id,
             message,
             null,
