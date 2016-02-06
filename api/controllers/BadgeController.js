979105b52b2c760b925493c4a872f1126c37c1a2
kmangla <karan.mangla@gmail.com>
2016-02-06 11:16:10 +0530

    Fix badge controller.

--- api/controllers/BadgeController.js
+++ api/controllers/BadgeController.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   getBadgeCount: function(req, res) {
+    var userID = req.session.User.id;
     Task.find({assignedBy: userID, status: 'open'}).exec(function (err, tasks) {
       var badgeCount = 0;
       for (var i = 0; i < tasks.length; i++) {
